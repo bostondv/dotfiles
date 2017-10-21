bb00120b159534f7e86df40d5fa30cf072303894
Boston Dell-Vandenberg <hello@bostondv.com>
2017-10-21 15:33:24 -0400

    Add react-native-cli

--- node/install.sh
+++ node/install.sh
@@ -7,5 +7,8 @@ npm i -g ios-deploy
 npm i -g ionic
 npm i -g create-react-app
 npm i -g create-react-native-app
+npm i -g eslint
+npm i -g prettier
+npm i -g react-native-cli
 
 echo " Successfully installed all npm dependencies.\n"
